1657d4bc47f351d313d0c0dd3e1c7b92b9529c08
Your Name~~ <you@example.com>
2022-12-31 11:17:29 +0530

    module errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import { MatDialog } from '@angular/material';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+// export class MyErrorStateMatcher implements ErrorStateMatcher {
+//   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+//     const isSubmitted = form && form.submitted;
+//     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+//   }
+// }
 
 @Component({
   selector: 'app-root',
@@ -7,4 +18,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RentPayCalculator';
+  amountFormControl = new FormControl('', [Validators.required, Validators.maxLength(8)]);
+  Rentamount:number = 0;
+  // matcher = new MyErrorStateMatcher();
+  RentpayData = [
+    {
+      "id": 1,
+      "name": "Redgirraffe",
+      "slug": "redgirraffe",
+      "logo": "https://redgirraffe.com/assets/images/RGVector.svg",
+      "fee": 0.39,
+      "GST": 18,
+      "RentalDocs": "10000"
+    },
+  ];
+
+  constructor(public dialog: MatDialog) { }
+
+
+  calculate(fc: AbstractControl){
+    console.log(fc.value);
+    this.Rentamount = Number(fc.value);
+  }
 }
